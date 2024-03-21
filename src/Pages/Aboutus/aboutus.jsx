43b055a9b2b4d05eeafff33eb115ae2ed0fbc3a6
rubanshiva <rubanshivaram@gmail.com>
2024-03-21 21:24:12 +0530

    aboutuspage

--- src/Pages/Aboutus/aboutus.jsx
+++ src/Pages/Aboutus/aboutus.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
+import React from "react";
+import Header from "../../Components/Header/Header";
 
 function aboutus() {
   return (
     <div>
-        <Header/>
+      <Header />
       <h1>About the company</h1>
-
+      <p>
+        Ease Investments Solution is registered under Indian Partnership Act,
+        1932 and is associated with SEBI registered research analyst under the
+        SEBI Regulations, 2014 as a research analyst (SEBI Registered Analyst
+        Number: INH000010803 / Principal officer Suhail Ahamed).
+      </p>
+      <p>
+        The Registered address of Ease Investments Solution is 1017, Vijaya Bank
+        Layout, Btm 4th stage, Bangalore, KA, 560076.
+      </p>
+      <h2>Operations</h2>
+      <ol>
+        <li> Ease Investments Solution charges the fees for mentorship services and the amount paid by subscribers is not for any investment, FD or Demat purpose.</li>
+        <li> Ease Investments Solution or any of its executives don’t execute any trade on customer’s behalf.</li>
+        <li> For Refunds, you can read the Refund Policy in the agreement signed by you with Ease Investments Solution.</li>
+        <li> Investment in the Stock Market is subject to market risk and your capital may be at risk. Ease Investments Solution or any of its executives doesn’t guarantee any pre-specified profits.</li>
+        <li> Ease Investments Solution or its employees would never call you and demand for any kind of sensitive details such as demat credentials or unsolicited trading advices You can report such Calls at support@easeinvsolution.com or 80-49587414.</li>
+        <li> Ease Investments Solution would never send you any small cap/penny stocks tip. So if you receive any SMS, Call or Mail regarding any such stock tips, kindly report at 80-49587414.</li>
+      </ol>
     </div>
-  )
+  );
 }
 
-export default aboutus
+export default aboutus;
