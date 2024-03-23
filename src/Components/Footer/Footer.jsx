7f3e46b0942ac4b8be6c871a89dce133eb656224
rubanshiva <rubanshivaram@gmail.com>
2024-03-23 12:19:36 +0530

    footeredits

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import logo from "../../Assets/images/Logo.jpg";
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
+    <div className='footer' >
+   <div className='footr1'>
     <div>
-      
+      <img  src={logo}
+      alt='logo'
+      style={{ width: "100%", height: "50px" }}  />
+    </div>
+    
+    <div>
+          <Link to='/' ><button>Home</button> </Link>
+          <Link to='/aboutus' >  <button>about</button> </Link>
+          <Link to='/trackrecord' >  <button>TrackRecord</button> </Link>
+          <Link to='/smecorner' >  <button>smecorner</button> </Link>        
+    </div>
+    
+    <div>
+        <button>facebook</button>
+        <button>linkadien</button>
+    </div>
+
+    </div>
+
     </div>
   )
 }
