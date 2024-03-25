b0614679226f04ffaf9e44a0d8a94ad75ccea9bc
rubanshiva <rubanshivaram@gmail.com>
2024-03-25 22:03:00 +0530

    inc

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Subplan from "./subplan";
+import "./home.css";
+import Carousel from "react-bootstrap/Carousel";
+import silder1 from "../../Assets/images/slider1.jpg";
+import slider2 from "../../Assets/images/slider2.jpg";
 
 function home() {
   return (
@@ -35,6 +40,31 @@ function home() {
           repudiandae alias omnis distinctio molestias a, ab velit cumque
           consectetur quisquam sed ipsam quis at quas, nemo quibusdam repellat?
           Ducimus, ex?
+        </p>
+        <Subplan/>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default home;
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import Subplan from "../Home/subplan"
+
+function home() {
+  return (
+    <div>
+      <Header />
+      <div className="maindiv">
+        <h1>welcome to home page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          repudiandae alias omnis distinctio molestias a, ab velit cumque
+          consectetur quisquam sed ipsam quis at quas, nemo quibusdam repellat?
+          Ducimus, ex?{" "}
         </p>
         <Subplan/>
       </div>
