6060938337e8fd778177bc338551ea93e7fd481d
rubanshiva <rubanshivaram@gmail.com>
2024-03-25 22:04:43 +0530

    mat

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -49,28 +49,3 @@ function home() {
 }
 
 export default home;
-import React from "react";
-import Header from "../../Components/Header/Header";
-import Footer from "../../Components/Footer/Footer";
-import Subplan from "../Home/subplan"
-
-function home() {
-  return (
-    <div>
-      <Header />
-      <div className="maindiv">
-        <h1>welcome to home page</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          repudiandae alias omnis distinctio molestias a, ab velit cumque
-          consectetur quisquam sed ipsam quis at quas, nemo quibusdam repellat?
-          Ducimus, ex?{" "}
-        </p>
-        <Subplan/>
-      </div>
-      <Footer />
-    </div>
-  );
-}
-
-export default home;
